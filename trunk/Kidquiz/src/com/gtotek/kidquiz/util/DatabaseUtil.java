e8b71002f4bf4fed3a2b2bc6fbae1b40cb40e340
trienvu135 <trienvu135@af304ce6-d493-56c7-7c20-ef83746a7358>
2014-10-01 07:52:13 +0000

    df

--- trunk/Kidquiz/src/com/gtotek/kidquiz/util/DatabaseUtil.java
+++ trunk/Kidquiz/src/com/gtotek/kidquiz/util/DatabaseUtil.java
@@ -6,7 +6,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseUtil extends SQLiteAssetHelper {
 	private static final String DATABASE_NAME = "kidquiz";
-	private static final int DATABASE_VERSION = 1;
+	private static final int DATABASE_VERSION = 2;
 
 	public DatabaseUtil(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
